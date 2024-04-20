d2a17b1b78cfe9ac80cae6477686d91b215125eb
eaemorgado <eaemorgado@gmail.com>
2024-04-20 17:56:36 -0300

    att

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Chamada de função para mostrar o primeiro slide
+showSlide(currentSlide);
+
